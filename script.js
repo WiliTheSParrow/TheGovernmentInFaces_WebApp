f2a1eb93e6f0f4cda871c700e9e63a43564a351c
WiliTheSParrow <w.julia91@gmail.com>
2019-05-10 13:49:05 +0200

    table cont

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function senateTable(){
-    var table = document.getElementById("table-data")
-};
+var table = document.getElementById("table-data")
+table.setAttribute("border", "1");
+var row = document.createElement("TR");
+var headerName = document.createElement("TH");
+headerName.innerHTML = "Senator";
+row.appendChild(headerName);
+table.appendChild(row);
+
+var headerParty = document.createElement("TH");
+headerParty.innerHTML = "Party Affiliation";
+row.appendChild(headerParty);
+table.appendChild(row);
+
+var headerState = document.createElement("TH");
+headerState.innerHTML = "State";
+row.appendChild(headerState);
+table.appendChild(row);
