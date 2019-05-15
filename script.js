b5765ca7f4037caca05fb8fb42dfb4dd6772f7da
WiliTheSParrow <w.julia91@gmail.com>
2019-05-15 14:46:58 +0200

    building up the table from JSON_states

--- script.js
+++ script.js
@@ -29,18 +29,34 @@ table.appendChild(row);
 
 for (i = 0; i < data.results[0].members.length; i++) {
 
+    //Senator names
     var row = document.createElement("TR");
     row.setAttribute("border", "1");
-
     var names = document.createElement("TD");
     if (data.results[0].members[i].middle_name === null) {
         names.innerHTML = data.results[0].members[i].first_name + " " + data.results[0].members[i].last_name;
     } else {
         names.innerHTML = data.results[0].members[i].first_name + " " + data.results[0].members[i].middle_name + " " + data.results[0].members[i].last_name;
     };
-
-
     names.setAttribute("class", "tablenames");
     row.appendChild(names);
     table.appendChild(row);
+
+    //Parties
+    var parties = document.createElement("TD");
+    parties.innerHTML = data.results[0].members[i].party;
+    parties.setAttribute("class", "tableparties");
+    row.appendChild(parties);
+    table.appendChild(row);
+    
+    //State
+    var states = document.createElement("TD");
+    states.innerHTML = data.results[0].members[i].state;
+    states.setAttribute("class", "tablestates");
+    row.appendChild(states);
+    table.appendChild(row);
+    
+    //Seniority
+    
+    //Vote percentage
 }
